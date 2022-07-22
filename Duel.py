077da585a782b4608176bf836dcda7a0537b12cc
Mateusz Meksuła <mateusz.meksula@gmail.com>
2022-07-22 22:15:37 +0200

    Program logic has been done

--- Duel.py
+++ Duel.py
@@ -35,13 +35,56 @@ class Character:
         self.health = health
 
 
-def play_again():
-    # this function ask user if he wants to play again
-    choice = input("\nPlay again? (yes/no): ").upper()
-    if choice == 'YES':
-        return True
-    else:
-        return False
+def make_choice(char_list):
+    # this function allows user to choose character from a list
+
+    # variable 'index' allows to display list indexes
+    index = 1
+    for char in char_list:
+        print(f"{index}: {char.name}")
+        index += 1
+
+    # returning characters list index of a list of characters
+    # other functions will require characters list index as an input
+    choice = input("Type corresponding number: ")
+    list_index = int(choice) - 1
+    return list_index
+
+
+def show_char_stats(char_list):
+    # this function allows user to see character attributes
+
+    # asking user to make a choice, which characters stats he wants to display
+    print("Which one?: ")
+    chosen_char = char_list[make_choice(char_list)]
+
+    # printing chosen character's attributes
+    print(f"Name: {chosen_char.name}")
+    print(f"Health: {chosen_char.health}")
+    print(f"Damage: {chosen_char.damage}")
+    print(f"Critical strike chance: {chosen_char.critical_strike_chance}")
+    print(f"Chance to dodge strike: {chosen_char.dodge_chance}")
+
+
+def match_making(char_list):
+    # this function allows user to choose two characters to fight each other
+
+    # creation of an empty list to later append two characters
+    fighting_characters = []
+
+    # removing first chosen character to make sure
+    # user can't choose same character again
+    print("Choose first fighter: ")
+    first_char = char_list.pop(make_choice(char_list))
+    fighting_characters.append(first_char)
+
+    # same as above, appending user choice to list
+    print("Choose second fighter: ")
+    second_char = char_list[make_choice(char_list)]
+    fighting_characters.append(second_char)
+
+    # returning list of two characters that will fight each other
+    return fighting_characters
 
 
 def fight(char_1, char_2):
@@ -117,17 +160,26 @@ def fight(char_1, char_2):
     char_2.restore_health(health_to_restore_2)
 
 
-# creation of characters for testing purposes
-test_character_1 = Character('josh', 200, [20, 25], 30, 30)
-test_character_2 = Character('mosh', 100, [15, 20], 50, 50)
-
-
 def main():
+    # creation of characters for testing purposes
+    test_character_1 = Character('josh', 200, [20, 25], 30, 30)
+    test_character_2 = Character('mosh', 100, [15, 20], 50, 50)
+    characters_list = [test_character_1, test_character_2]
 
-    # main game algorithm - needs to be developed
-    fight(test_character_1, test_character_2)
-    while play_again():
-        fight(test_character_1, test_character_2)
+    while True:
+        print("What do you want to do?")
+        print("1: Show stats")
+        print("2: Play")
+        print("any key: quit")
+
+        choice = input('Type corresponding number: ')
+        if choice == '1':
+            show_char_stats(characters_list)
+        elif choice == '2':
+            fighting_chars = match_making(characters_list[:])
+            fight(fighting_chars[0], fighting_chars[1])
+        else:
+            break
 
 
 if __name__ == '__main__':
